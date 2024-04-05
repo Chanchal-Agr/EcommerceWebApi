5f4174756e5d42cd7e1fcb76d6d1933fb3f14490
Vishal Shrivas <VShrivas@daenet.com>
2024-04-05 19:49:38 +0530

    Create Frontend blazor

---
+++ OnlineShoppingE-CommerceApplication.Frontend/Program.cs
@@ -0,0 +1,19 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using OnlineShoppingE_CommerceApplication.Frontend;
+using OnlineShoppingECommerceApplication.Frontend.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<CustomAuthStateProvider>());
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
---
+++ OnlineShoppingE-CommerceApplication.Frontend/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,49 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace OnlineShoppingECommerceApplication.Frontend.Services
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService localstorage;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            localstorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await localstorage.GetItemAsync<string>("Token");
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+            else
+            {
+                var claims = new[]
+                {
+                new Claim(ClaimTypes.Name, "user"),
+                new Claim(ClaimTypes.Role, "admin")
+            };
+                var identity = new ClaimsIdentity(claims, "jwt");
+                var user = new ClaimsPrincipal(identity);
+                return new AuthenticationState(user);
+            }
+        }
+
+        public async Task MarkUserAsAuthenticated(string token)
+        {
+            await localstorage.SetItemAsync("Token", token);
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+            await localstorage.RemoveItemAsync(token);
+        }
+
+        public async Task MarkUserAsLoggedOut()
+        {
+            await localstorage.RemoveItemAsync("Token");
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
